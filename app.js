5d2d9944731f74939f45650057ddfb83725801e6
Tony Spiro <tspiro@tonyspiro.com>
2017-07-31 10:02:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ app.set('port', (process.env.PORT || 3000))
 app.use('/', express.static(__dirname + '/public/'))
 var Cosmic = require('cosmicjs')
 var bucket_slug = process.env.COSMIC_BUCKET || 'stellar'
+var read_key = process.env.COSMIC_READ_KEY
 app.get('/', function(req, res) {
-  Cosmic.getObjects({ bucket: { slug: bucket_slug } }, function(err, response) {
+  Cosmic.getObjects({ bucket: { slug: bucket_slug, read_key: read_key } }, function(err, response) {
     res.locals.cosmic = response;
     res.render('index.html')
   })
